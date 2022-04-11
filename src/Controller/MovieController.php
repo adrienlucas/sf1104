f147412505c5b7255ae98d0fe44bf698dab2f113
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-04-11 16:50:25 +0200

    Add bootstrap and style the pages

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Cassandra\Date;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +18,11 @@ class MovieController extends AbstractController
     {
         return $this->render('movie/show.html.twig', [
             'movie_id' => $id,
+            'movie'=> [
+                'title' => 'foobar',
+                'description' => str_repeat('foobar ', 20),
+                'release_date' => new DateTime('2020-01-01')
+            ]
         ]);
     }
 }
