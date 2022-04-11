a55122e1ecae8698a2395229c2d4d9e1a2e89c80
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-04-11 17:32:26 +0200

    Improve the layout

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -15,4 +15,13 @@ class HomepageController extends AbstractController
     {
         return $this->render('homepage/index.html.twig');
     }
+
+    /**
+     * Deprecated, use direct twig include of the template
+     * @Route("/_fragment/navbar", name="app_navbar")
+     */
+    public function menu(): Response
+    {
+        return $this->render('navbar.html.twig');
+    }
 }
